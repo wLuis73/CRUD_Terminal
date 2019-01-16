6f2172b5db49f0d293a97a14a7100fbd3a10290e
wLuis73 <webersonlr@gmail.com>
2019-01-16 18:29:44 -0200

    Composer Autoload Setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# CRUD_Terminal" 
+Testes 160119, 18h14
---
+++ tutorial.py
@@ -0,0 +1,22 @@
+import sqlite3, os, time, winsound
+
+class Manager:
+    def __init__(self):
+        self.name = ''
+        self.phone = ''
+        self.address = ''
+
+    def add(self):
+        pass
+    
+    def update(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_list(self):
+        pass
+
+    def terminate(self):
+        pass
