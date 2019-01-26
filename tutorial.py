975ff98a57e14720bfbc744652b38a4d503219de
wLuis73 <webersonlr@gmail.com>
2019-01-26 12:58:05 -0200

    Novos testes 260119 - 12h57

--- tutorial.py
+++ tutorial.py
@@ -29,7 +29,7 @@ class Manager:
         print('2 - Update')
         print('3 - Remove')
         print('4 - List')
-        print('5 - End')        
+        print('5 - End')                   
 
     def main(self):
         self.menu()
